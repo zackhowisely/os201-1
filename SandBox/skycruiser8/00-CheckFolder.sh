6fe93a6a2eb8836bd1977e1b0efdd835255bfa9f
Rafi Muhammad Daffa <rafi.daffa@gmail.com>
2020-03-29 12:42:24 +0700

    skycruiser8 - Add checking scripts
    Signed-off-by: Rafi Muhammad Daffa <rafi.daffa@gmail.com>

---
+++ SandBox/skycruiser8/00-CheckFolder.sh
@@ -0,0 +1,25 @@
+# 00-CheckFolder.sh
+# ============================================================================
+# Author: Rafi Muhammad Daffa (skycruiser8)
+#
+# INFO:
+# This script lists the directories inside the parent directory
+# (above the current directory).
+#
+# VERSION:
+# v0 (2020-03-29 12:12 UTC+7) - Initial commit
+#
+# This is free software :)
+
+DIRLIST=$(ls -d ../*/ | sed -e 's/\///g' -e 's/\.\.//g')
+
+[[ -e sandbox-201.txt ]] && rm -f sandbox-201.txt
+
+touch sandbox-201.txt
+
+for II in $DIRLIST
+do
+echo $II >> sandbox-201.txt
+done
+
+# EOF
---
+++ SandBox/skycruiser8/01-CheckGithubPages.sh
@@ -0,0 +1,43 @@
+# 01-CheckGithubPages.sh
+# ============================================================================
+# Author: Rafi Muhammad Daffa (skycruiser8)
+#
+# INFO:
+# This script continues on the work made by 00-CheckFolder.sh to check
+# whether a Github user whose username is the name of a folder here has an
+# os201 Github Pages site.
+#
+# VERSION:
+# v0 (2020-03-29 12:30 UTC+7) - Initial commit
+#
+# This is free software :)
+
+CFSCRIPT="00-CheckFolder.sh"
+CFOUT="sandbox-201.txt"
+CGPOUT="GitHub-Pages-os201.txt"
+
+FNOTFOUNDMSG="$CFSCRIPT is required but cannot be found"
+CFERRORMSG="$CFOUT not found. Please use original 00-CheckFolder.sh"
+
+[[ -e $CFSCRIPT ]] || (echo $FNOTFOUNDMSG;exit 0)
+
+bash $CFSCRIPT
+
+[[ -e $CFOUT ]] || (echo $CFERRORMSG;exit 0)
+
+UNAMELIST=$(cat $CFOUT)
+
+[[ -e $CGPOUT ]] && rm -f $CGPOUT
+
+touch $CGPOUT
+
+for II in $UNAMELIST
+do
+RESPONSE=$(curl "https://$II.github.io/os201/")
+if [ "Site  not found" == "*$RESPONSE*" ]
+then
+echo "$II 0" >> $CGPOUT
+else
+echo "$II 1" >> $CGPOUT
+fi
+done
