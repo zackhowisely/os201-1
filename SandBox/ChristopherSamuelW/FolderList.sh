398df1be2ccf70b20a95440aebf48ef00d2adac9
ChristopherSamuelW <60346810+ChristopherSamuelW@users.noreply.github.com>
2020-03-31 13:32:36 +0700

    Update FolderList.sh

--- SandBox/ChristopherSamuelW/FolderList.sh
+++ SandBox/ChristopherSamuelW/FolderList.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # INFO : Mencari dan menuliskan semua nama folder di os201/SandBox
 
-dirs = $(ls -d ../*/)
-FILE = sandbox-201.txt
-if [ -f FILE ];
-  then rm FILE
+dirs=$(ls -d ../*/)
+FILE=sandbox-201.txt
+if [ -f $FILE ];
+  then rm $FILE
 fi
 
-touch FILE
+touch $FILE
 
 for dir_name in $dirs
 do
-  echo $dir_name >> FILE
+  SIZE=${#dir_name}
+  SIZE=$(($SIZE-2))
+  echo ${dir_name:3} | cut -f1 -d"/" >> $FILE
 done
