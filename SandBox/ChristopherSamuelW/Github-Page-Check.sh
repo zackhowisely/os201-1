db25fda255ca6bffabb20333a79734eb166cb3ce
Christopher Samuel <samwinar@gmail.com>
2020-03-31 13:54:46 +0700

    Added script to check GitHub Page + output

---
+++ SandBox/ChristopherSamuelW/Github-Page-Check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# INFO : Memeriksa GitHub Pages ada atau tidak
+
+FILE=sandbox-201.txt
+OUTPUT=GitHub-Pages-os201.txt
+
+if [ -f $OUTPUT ];
+  then rm $OUTPUT
+fi
+
+cat $FILE | while read II
+  do
+	URL=https://$II.github.io/os201/
+    if (curl -s  "$URL" | grep "Site not found");
+    then
+       echo "$II 0" >> $OUTPUT 
+	else
+	   echo "$II 1" >> $OUTPUT 
+    fi
+  done
