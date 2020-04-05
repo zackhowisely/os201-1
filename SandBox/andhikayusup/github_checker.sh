f6bee2bd5f7a6882b71c78d39cd9e0acfe098a60
andhikayusup <andeeka.ym@gmail.com>
2020-04-05 10:32:17 +0700

    adding github_checker

---
+++ SandBox/andhikayusup/github_checker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FILE="GitHub-Pages-os201.txt"
+echo -n '' > $FILE
+
+for II in $(cat vanilla.txt)
+do
+    [ -d ../$II ] && {
+        printf '%-25s1\n' "$II" >> $FILE
+    }  
+    [ -d ../$II ] || {
+        printf '%-25s0\n' "$II" >> $FILE 
+    }
+done
+
