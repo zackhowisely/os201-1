a9c3a26edad040103fd36553ddc8acff37cc05c5
codefire53 <krisnaihsani@gmail.com>
2020-04-05 01:43:32 -0700

    Initial commit

---
+++ SandBox/codefire53/gitpage-check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# INFO : Memeriksa GitHub Pages ada atau tidak
+# Author : Mahardika Krisna Ihsani (codefire53)
+
+INPUT=sandbox-201.txt
+OUTPUT=validation-result.txt
+touch $OUTPUT;
+
+cat $INPUT || while read USER
+    do
+        URL="https://$USER.github.io/os201/"
+        [curl -s  "$URL" | grep "Site not found"] && echo "$USER 0" >> $OUTPUT;
+        [curl -s  "$URL" | grep "Site not found"] || echo "$USER 1" >> $OUTPUT 
+    done
