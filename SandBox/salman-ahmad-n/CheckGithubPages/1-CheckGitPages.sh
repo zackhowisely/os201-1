ea15b53282a4e2a79f6c02dbad743caae6ee4b3f
salman-ahmad-n <salman.nurhoiriza@gmail.com>
2020-04-06 16:50:19 +0700

    Finihing touches and run both scripts

--- SandBox/salman-ahmad-n/CheckGithubPages/1-CheckGitPages.sh
+++ SandBox/salman-ahmad-n/CheckGithubPages/1-CheckGitPages.sh
@@ -14,8 +14,8 @@ touch $OUTPUT
 cat $INPUT | 
 	while read username
 	do
-		STATUS=$(curl -I $username.github.io/os201/ | grep '^HTTP/\' | awk '{print $2}')
-		if [[ STATUS -eq 301 ]]
+		STATUS=$(curl -o /dev/null -Isw '%{http_code}' -m 30 https://$username.github.io/os201/)
+		if [[ STATUS -ne 404 ]]
 		then printf "%-25s 1\n" $username >> $OUTPUT
 		else printf "%-25s 0\n" $username >> $OUTPUT
 		fi
