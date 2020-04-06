4a0ea32f593873a04c8ce87b487d95b4577eec9e
salman-ahmad-n <salman.nurhoiriza@gmail.com>
2020-04-06 16:08:32 +0700

    Initial commit

---
+++ SandBox/salman-ahmad-n/CheckGithubPages/0-ListGitUsername.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# INFO : Membuat file yang berisi list dari directory sebelumya
+# Author : Salman Ahmad Nurhoiriza (salman-ahmad-n)
+
+ls -l ../.. | gawk '/^d/ {print $9}' > 00-sandbox-201.txt
