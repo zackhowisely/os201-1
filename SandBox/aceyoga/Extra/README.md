af076a7f5ad6022bb5a8560314d10a8662101f58
Yoga Mahendra <m.yoga.mahendra@gmail.com>
2020-04-09 15:26:05 +0700

    Updated batch scripts

--- SandBox/aceyoga/Extra/README.md
+++ SandBox/aceyoga/Extra/README.md
@@ -13,7 +13,7 @@
   - **Note that the name resembles to it's function.**
   - `createvenv.bat`
   - `startenv.bat`
-  - `createproject.bat` (Note: place the requirements.txt in the same folder as the batch file.)
+  - `createproject.bat`
   - `migrate.bat`
   - `testproject.bat`
 - Spring boot - Gradle related
--- SandBox/aceyoga/Extra/createproject.bat
+++ SandBox/aceyoga/Extra/createproject.bat
@@ -1,16 +1,10 @@
 @echo off
 set /p input="Write your project name: "
+set /p appname="Write your starting app name: "
 @echo on
 django-admin startproject %input%
 @echo off
 cd %input%
-set /p appname="Write your starting app name: "
-@echo on
-django-admin startapp %appname%
 @echo off
-echo Moving requirements.txt from ppw database.
-copy "C:\Users\Yoga Mahendra\Documents\Gitlab\django batch collections\requirements.txt" "%CD%"
-copy "C:\Users\Yoga Mahendra\Documents\Gitlab\django batch collections\datas\*" "%CD%"
-@echo on
-pip install -r requirements.txt
+django-admin startapp %appname%
 echo Your Project named %input% with App %appname% has been successfully created!
