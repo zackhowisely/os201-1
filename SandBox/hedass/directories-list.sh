9f5ee445b443dcf7bd1af5b755e3a59755a12896
Sean Urian <44487416+hedass@users.noreply.github.com>
2020-05-27 08:43:08 +0700

    Create directories-list.sh

---
+++ SandBox/hedass/directories-list.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# INFO : Mendaftarkan folder yang ada.
+# USAGE : ./directories-list.sh [DIR]
+
+DIR='..'
+OUTFILE='sandbox-201.txt'
+
+if [[ -e $1 ]]; then
+    DIR = $1
+fi
+
+list=$(ls $DIR -l | grep ^d | cut -d ' ' -f9)
+
+echo "$list" > $OUTFILE
+
+echo "File $OUTFILE telah dibuat dengan input dari $DIR"
